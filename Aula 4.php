d8134a9429aaa6ad4efffc7e56a1042bb7f7d684
Ester <139085601+ester-17@users.noreply.github.com>
2025-03-07 21:26:20 -0300

    Create Aula 4.php

---
+++ Aula 4.php
@@ -0,0 +1,24 @@
+<?php
+class Rectangle {
+    private $length;
+    private $width;
+
+    public function __construct($length, $width) {
+        $this->length = $length;
+        $this->width = $width;
+    }
+
+    public function getArea() {
+        return $this->length * $this->width;
+    }
+
+    public function getPerimeter() {
+        return 2 * ($this->length + $this->width);
+    }
+}
+
+$rectangle = new Rectangle(12, 9);
+
+echo "Area: " . $rectangle->getArea() . "</br>";
+echo "Perimeter: " . $rectangle->getPerimeter() . "</br>";
+?>
